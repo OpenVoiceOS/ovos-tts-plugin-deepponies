42d30a28d6a189384b5d41bf34dfe983d561e073
jarbasai <jarbasai@mailfence.com>
2023-03-14 23:50:34 +0000

    fix versions

--- setup.py
+++ setup.py
@@ -6,6 +6,31 @@ from setuptools import setup
 BASEDIR = os.path.abspath(os.path.dirname(__file__))
 
 
+def get_version():
+    """ Find the version of the package"""
+    version = None
+    version_file = os.path.join(BASEDIR, 'ovos_tts_plugin_google_tx', 'version.py')
+    major, minor, build, alpha = (None, None, None, None)
+    with open(version_file) as f:
+        for line in f:
+            if 'VERSION_MAJOR' in line:
+                major = line.split('=')[1].strip()
+            elif 'VERSION_MINOR' in line:
+                minor = line.split('=')[1].strip()
+            elif 'VERSION_BUILD' in line:
+                build = line.split('=')[1].strip()
+            elif 'VERSION_ALPHA' in line:
+                alpha = line.split('=')[1].strip()
+
+            if ((major and minor and build and alpha) or
+                    '# END_VERSION_BLOCK' in line):
+                break
+    version = f"{major}.{minor}.{build}"
+    if alpha and int(alpha) > 0:
+        version += f"a{alpha}"
+    return version
+
+
 def package_files(directory):
     paths = []
     for (path, directories, filenames) in os.walk(directory):
@@ -30,7 +55,7 @@ SAMPLE_CONFIGS = 'ovos-tts-plugin-deepponies.config = ovos_tts_plugin_deepponies
 
 setup(
     name='ovos-tts-plugin-deepponies',
-    version='0.0.1',
+    version=get_version(),
     description='deepponies tts plugin for OpenVoiceOS',
     url='https://github.com/OpenVoiceOS/ovos-tts-plugin-deepponies',
     author='JarbasAi',
@@ -41,21 +66,13 @@ setup(
     include_package_data = True,
     zip_safe=True,
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'Topic :: Text Processing :: Linguistic',
-        'License :: OSI Approved :: Apache Software License',
-
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.0',
-        'Programming Language :: Python :: 3.1',
-        'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Text Processing :: Linguistic",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
     keywords='mycroft plugin tts OVOS OpenVoiceOS',
     entry_points={'mycroft.plugin.tts': PLUGIN_ENTRY_POINT,
